2dc7051e89fffddcebc632811acb321a39bf9c43
Martin Garbe <monomartin@opennet-initiative.de>
2022-01-04 19:42:37 +0100

    olsrd repo moved to new location

--- build-olsrd-in-container.sh
+++ build-olsrd-in-container.sh
@@ -9,7 +9,7 @@ git checkout 4973feb538b5b98b9d8ac2f8f474202f6d73de78
 #
 # build .deb
 #
-git remote add debbuild https://github.com/leoss/olsrd-debian-package.git
+git remote add debbuild https://github.com/opennet-initiative/olsrd-debian-package.git
 git fetch debbuild
 git checkout debbuild/debian_package debian
 #vi debian/changelog # Set the version number on top line (optional)
