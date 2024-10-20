e6459098f7e1c6d67cae4016fd3efc419d258e9f
Martin Garbe <monomartin@opennet-initiative.de>
2024-10-20 17:49:40 +0200

    Adapt instruction to olsrd repo update

--- build-olsrd-in-container.sh
+++ build-olsrd-in-container.sh
@@ -4,8 +4,7 @@
 cd /olsrd/temp
 git clone https://github.com/opennet-initiative/olsrd/
 cd olsrd/
-git checkout 2acf4758ac1e4786a524d7ce2ed95b6cc49faffa
 
-dpkg-buildpackage -uc -us
+dpkg-buildpackage -b -uc -us
 
 cp ../*.deb /olsrd/mount/
